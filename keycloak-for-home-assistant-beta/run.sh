5bc0945f8bde7491ecd9ef2086c2f04e143e2cd7
Michaël van der Veen <pjvanderveen@msn.com>
2025-07-06 16:16:06 +0200

    Update dockerfile

--- keycloak-for-home-assistant-beta/run.sh
+++ keycloak-for-home-assistant-beta/run.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/with-contenv bashio
+
+export KC_BOOTSTRAP_ADMIN_USERNAME=$(bashio::config 'KC_BOOTSTRAP_ADMIN_USERNAME')
+export KC_BOOTSTRAP_ADMIN_PASSWORD=$(bashio::config 'KC_BOOTSTRAP_ADMIN_PASSWORD')
 
 /opt/keycloak/bin/kc.sh start-dev \
   --http-port=8080 \
