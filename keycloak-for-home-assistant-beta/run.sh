7a3efbf86c92f22705a39109b089a26ce3925b34
Michaël van der Veen <pjvanderveen@msn.com>
2025-07-06 23:47:19 +0200

    fix typo in run

--- keycloak-for-home-assistant-beta/run.sh
+++ keycloak-for-home-assistant-beta/run.sh
@@ -2,7 +2,7 @@
 
 export KC_BOOTSTRAP_ADMIN_USERNAME=$(bashio::config 'KC_BOOTSTRAP_ADMIN_USERNAME')
 export KC_BOOTSTRAP_ADMIN_PASSWORD=$(bashio::config 'KC_BOOTSTRAP_ADMIN_PASSWORD')
-export KC_HTTP_ENABLED: true
+export KC_HTTP_ENABLED=true
 
 /opt/keycloak/bin/kc.sh start-dev \
   --http-port=8080 \
