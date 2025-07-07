c3a8e541b71d63e0feb0835e0d7291ce47845112
Michaël van der Veen <pjvanderveen@msn.com>
2025-07-07 21:46:08 +0200

    Add database support

--- keycloak-for-home-assistant-beta/run.sh
+++ keycloak-for-home-assistant-beta/run.sh
@@ -8,4 +8,9 @@ export KC_HTTP_ENABLED=true
   --http-port=8080 \
   --hostname-strict=false \
   --http-enabled=true \
-  --hostname $(bashio::config 'HOSTNAME')
+  --hostname $(bashio::config "HOSTNAME") \
+  --db $(bashio::config "DATABASE_TYPE") \
+  --db-username $(bashio::services "mysql" "username") \
+  --db-password $(bashio::services "mysql" "password") \
+  --db-url-host $(bashio::services "mysql" "host") \
+  --db-url-port $(bashio::services "mysql" "port")
