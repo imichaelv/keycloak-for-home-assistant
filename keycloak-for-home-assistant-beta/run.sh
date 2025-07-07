988e316d87a5072c5f43d66bfb61d7acca925d64
Michaël van der Veen <pjvanderveen@msn.com>
2025-07-07 22:03:37 +0200

    Create keycloak database upon first run

--- keycloak-for-home-assistant-beta/run.sh
+++ keycloak-for-home-assistant-beta/run.sh
@@ -4,6 +4,13 @@ export KC_BOOTSTRAP_ADMIN_USERNAME=$(bashio::config 'KC_BOOTSTRAP_ADMIN_USERNAME
 export KC_BOOTSTRAP_ADMIN_PASSWORD=$(bashio::config 'KC_BOOTSTRAP_ADMIN_PASSWORD')
 export KC_HTTP_ENABLED=true
 
+bashio::log.info "Creating 'keycloak' database if not present..."
+mysql -h $(bashio::services "mysql" "host") \
+  -P $(bashio::services "mysql" "port") \
+  -u $(bashio::services "mysql" "username") \
+  -p $(bashio::services "mysql" "password") \
+  -e "CREATE DATABASE IF NOT EXISTS keycloak CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+
 /opt/keycloak/bin/kc.sh start \
   --http-port=8080 \
   --hostname-strict=false \
