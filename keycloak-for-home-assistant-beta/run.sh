8db277549e3798b4f124df99aeb3c3fe3402560f
Michaël van der Veen <pjvanderveen@msn.com>
2025-07-07 21:51:53 +0200

    Enable prod build

--- keycloak-for-home-assistant-beta/run.sh
+++ keycloak-for-home-assistant-beta/run.sh
@@ -4,7 +4,7 @@ export KC_BOOTSTRAP_ADMIN_USERNAME=$(bashio::config 'KC_BOOTSTRAP_ADMIN_USERNAME
 export KC_BOOTSTRAP_ADMIN_PASSWORD=$(bashio::config 'KC_BOOTSTRAP_ADMIN_PASSWORD')
 export KC_HTTP_ENABLED=true
 
-/opt/keycloak/bin/kc.sh start-dev \
+/opt/keycloak/bin/kc.sh start \
   --http-port=8080 \
   --hostname-strict=false \
   --http-enabled=true \
